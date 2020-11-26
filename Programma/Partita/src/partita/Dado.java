555b00bac6a04a16606dedd6fdde81e62ec25782
Maurizio Morselli <morsellimaurizio@istitutocorni.it>
2020-11-26 23:02:55 +0100

    Riaggiunto tutto

---
+++ Programma/Partita/src/partita/Dado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partita;
+
+/**
+ *
+ * @author famig
+ */
+public class Dado {
+    
+}
