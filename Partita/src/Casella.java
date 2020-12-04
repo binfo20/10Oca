9d4b8c760f757d98a6026aff81bd9173bb07b06a
Manuel-michelini <72730725+Manuel-michelini@users.noreply.github.com>
2020-12-04 12:13:39 +0100

    tostring

--- Partita/src/Casella.java
+++ Partita/src/Casella.java
@@ -1,6 +1,11 @@
 import java.io.Serializable;
 
 public class Casella  implements Serializable {
+    String casella;
 
 
-}
+    public String toString() {
+        return "Casella{" +
+                "casella='" + casella ;};
+    }
+
