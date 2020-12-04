c547852f45982f952c7dddeb45f378524943acba
Maurizio Morselli <morsellimaurizio@istitutocorni.it>
2020-12-04 11:32:42 +0100

    Update dado

--- Partita/src/Dado.java
+++ Partita/src/Dado.java
@@ -2,11 +2,10 @@ import java.io.Serializable;
 import java.util.Random;
 
 public class Dado implements Serializable {
-        private short facce = 6;
-        generatore = new Random();
+    private short facce = 6;
+    private Random generatore = new Random();
     public int lancia(){
         return 1 + generatore.nextInt(facce);
     }
-    private Random generatore;
-    private int facce;
+
 }
--- Partita/src/Partita.java
+++ Partita/src/Partita.java
@@ -5,6 +5,7 @@ public class Partita implements Serializable {
     public static void main (String[] args) {
         System.out.println("Sono il Main!");
         Giocatore g = new Giocatore("Mario");
+        Dado d = new Dado();
         Impostazioni i = new Impostazioni();
         System.out.println(i.Salva(g));
     }
