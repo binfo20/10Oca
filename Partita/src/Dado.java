1f37b22094e6abaf9a53a07fcdc90fba4d6c81dd
Maurizio Morselli <morsellimaurizio@istitutocorni.it>
2020-12-04 11:35:24 +0100

    Update Dado,Uml

--- Partita/src/Dado.java
+++ Partita/src/Dado.java
@@ -4,6 +4,7 @@ import java.util.Random;
 public class Dado implements Serializable {
     private short facce = 6;
     private Random generatore = new Random();
+
     public int lancia(){
         return 1 + generatore.nextInt(facce);
     }
