932d32fe10819cb442cdf177570093fc279da16e
Gianluca-18 <71022989+Gianluca-18@users.noreply.github.com>
2020-12-04 10:58:26 +0100

    Update Dado.java

--- Partita/src/Dado.java
+++ Partita/src/Dado.java
@@ -1,4 +1,18 @@
 import java.io.Serializable;
+import java.util.Random;
 
 public class Dado implements Serializable {
+    public Dado(int s){
+        facce = s;
+        generatore = new Random();
+    }
+    public Dado(){
+        facce = 4;
+        generatore = new Random();
+    }
+    public int lancia(){
+        return 1 + generatore.nextInt(facce);
+    }
+    private Random generatore;
+    private int facce;
 }
