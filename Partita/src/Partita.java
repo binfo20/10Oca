1a348226db67d642a8f5aa959d36b4e46d3e1ba9
Prof. Missiroli (home) <prof.missiroli@gmail.com>
2020-12-06 13:00:04 +0100

    Commenti prof 6/12

--- Partita/src/Partita.java
+++ Partita/src/Partita.java
@@ -1,6 +1,16 @@
 import java.io.Serializable;
 import static java.lang.System.*;
 
+/***
+ * MI pare andiamo bene.
+ * Manca il javadoc
+ * Togliete i file .class dal controllo di git, è inutile e spreca banda
+ * (cfr. gitignore)
+ * Riguardardate l'idea "CasellaSpecilae". Al momento non c'è tatnto,
+ * serve solo a marcare la sua esistenza (e potete usare .getClass() senza
+ * creare il metodo isSpeciale()
+ */
+
 public class Partita implements Serializable {
 
     public static void main (String[] args) {
